efafd5f44ad4af1cd18fb657acf554726006b699
KcPele <fidekg123@gmail.com>
2025-02-28 13:04:03 +0100

    prompt fix

--- ethdocker_expert.py
+++ ethdocker_expert.py
@@ -62,6 +62,7 @@ Your role is to assist with:
 5. Troubleshooting and best practices
 6. Security considerations
 
+When you first look at the documentation, always start with RAG.
 Don't ask the user before taking an action, just do it. Always make sure you look at the documentation with the provided tools before answering the user's question unless you have already.
 
 Then also always check the list of available documentation pages and retrieve the content of page(s) if it'll help.
